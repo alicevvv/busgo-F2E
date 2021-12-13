3513de75f5d731ff1cb91fdab1008658af037292
Alice <alicev871203@gmail.com>
2021-12-13 15:02:59 +0800

    api fix

--- src/api/index.js
+++ src/api/index.js
@@ -17,12 +17,14 @@ function GetAuthorizationHeader(){
     var AppId = '04de29132d8b46229bfbcf8f78ef2c37';
     var AppKey = '1A6Q1F8RFdo8WlqCsZm0jwlDWEo';
 
-    var GMTString = new Date().toGMTString;
+    var GMTString = new Date().toGMTString();
     var ShaObj = new jsSHA("SHA-1","TEXT");
     ShaObj.setHMACKey(AppKey,'TEXT');
     ShaObj.update('x-date: '+ GMTString);
     var HMAC = ShaObj.getHMAC("B64");
-    var Autorization = 'hmac username="'+AppId+'",algorithm="hmac-sha1,headers="x-date",signature="'+HMAC+'"';
+    var Autorization = 'hmac username="' + AppId + '", algorithm="hmac-sha1", headers="x-date", signature="' + HMAC + '"';
+    console.log('Autorization');
+    console.log(Autorization);
     return{
         Authorization: Autorization,
         "X-Date":GMTString,
